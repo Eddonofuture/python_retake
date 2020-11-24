87aa442e6afe77a3444cf160fcf596f1815d455b
Felipe Veloso <f.veloso.n@gmail.com>
2020-11-24 11:51:52 -0300

    avances

---
+++ property/ej3.py
@@ -0,0 +1,19 @@
+import sys
+import shutil
+import zipfile
+from pathlib import Path
+
+class ZipReplace:
+    def __init__(self, filename, search_string, replace_string):
+        self.filename = filename
+        self.search_string = search_string
+        self.replace_string = replace_string
+        self.temp_directory = Path(f"unzipped-{filename}")
+
+    def zip_find_replace(self):
+        self.unzip_files()
+        self.find_replace()
+        self.zip_files()
+
+    def unzip_files(self):
+        self.temp_directory.mkdir8
